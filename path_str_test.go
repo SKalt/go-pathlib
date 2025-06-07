0514c29087c20393f014760dcc9df0afa997f2f4
Steven Kalt <kalt.steven@gmail.com>
2025-06-07 13:58:47 +0000

    chore: implement some examples for PathStr

---
+++ path_str_test.go
@@ -0,0 +1,57 @@
+package pathlib_test
+
+import (
+	"fmt"
+
+	"github.com/skalt/go-pathlib"
+)
+
+func ExamplePathStr_IsLocal() {
+	p := pathlib.PathStr("/absolute/path/to/file.txt")
+	fmt.Println(p.IsLocal()) // false
+
+	p = pathlib.PathStr("relative/path/to/file.txt")
+	fmt.Println(p.IsLocal()) // true
+
+	p = pathlib.PathStr("./local/path/to/file.txt")
+	fmt.Println(p.IsLocal()) // true
+	// Output:
+	// false
+	// true
+	// true
+}
+func ExamplePathStr_Join() {
+	fmt.Println("On Unix:")
+	fmt.Println(pathlib.PathStr("a").Join("b", "c"))
+	fmt.Println(pathlib.PathStr("a").Join("b/c"))
+	fmt.Println(pathlib.PathStr("a/b").Join("c"))
+	fmt.Println(pathlib.PathStr("a/b").Join("/c"))
+
+	fmt.Println(pathlib.PathStr("a/b").Join("../../../xyz"))
+	// Output:
+	// On Unix:
+	// a/b/c
+	// a/b/c
+	// a/b/c
+	// a/b/c
+	// ../xyz
+}
+
+func ExamplePathStr_IsAbsolute() {
+	fmt.Println("On Unix:")
+	fmt.Println(pathlib.PathStr("/home/gopher").IsAbsolute())
+	fmt.Println(pathlib.PathStr(".bashrc").IsAbsolute())
+	fmt.Println(pathlib.PathStr("..").IsAbsolute())
+	fmt.Println(pathlib.PathStr(".").IsAbsolute())
+	fmt.Println(pathlib.PathStr("/").IsAbsolute())
+	fmt.Println(pathlib.PathStr("").IsAbsolute())
+
+	// Output:
+	// On Unix:
+	// true
+	// false
+	// false
+	// false
+	// true
+	// false
+}
