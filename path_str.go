0c330c72f41425089a6ff028eda486e8b01974d2
Steven Kalt <kalt.steven@gmail.com>
2025-06-14 19:35:39 +0000

    refactor: split path_str.go into sections by interface

--- path_str.go
+++ path_str.go
@@ -9,6 +9,9 @@ import (
 
 type PathStr string
 
+// Beholder --------------------------------------------------------------------
+var _ Beholder[PathStr] = PathStr(".")
+
 // Note: go's `os.Stat/Lstat` imitates `stat(2)` from POSIX's libc spec.
 
 // Stat implements Beholder.
@@ -21,13 +24,6 @@ func (p PathStr) Stat() (OnDisk[PathStr], error) {
 func (p PathStr) Lstat() (OnDisk[PathStr], error) {
 	return p.OnDisk()
 }
-
-var (
-	_ PurePath             = PathStr(".")
-	_ Transformer[PathStr] = PathStr(".")
-	_ Readable[any]        = PathStr(".")
-	_ Beholder[PathStr]    = PathStr(".")
-)
 
 func (p PathStr) OnDisk() (actual OnDisk[PathStr], err error) {
 	var info os.FileInfo
@@ -46,99 +42,8 @@ func (p PathStr) Exists() (exists bool) {
 	return !errors.Is(err, fs.ErrNotExist)
 }
 
-// Returns true if the path is absolute, false otherwise.
-// See [filepath.IsAbs] for more details.
-func (p PathStr) IsAbsolute() bool {
-	return filepath.IsAbs(string(p))
-}
-
-// returns true if the path is local/relative, false otherwise.
-// see [filepath.IsLocal] for more details.
-func (p PathStr) IsLocal() bool {
-	return filepath.IsLocal(string(p))
-}
-
-func (p PathStr) Read() (result any, err error) {
-	// can't define this switch as a method of OnDisk[P] since OnDisk[P] has to handle
-	// any kind of path
-	var actual OnDisk[PathStr]
-	actual, err = p.OnDisk()
-	if err != nil {
-		return
-	}
-	mode := actual.Mode()
-
-	if mode.IsRegular() {
-		result, err = os.ReadFile(string(p))
-	} else if mode.IsDir() {
-		result, err = Dir(p).Read()
-	} else if isSymLink(mode) {
-		result, err = Symlink(p).Read()
-	} else if isCharDevice(mode) {
-		// TODO: CharDevice
-	} else if isDevice(mode) {
-		// TODO: BlockDevice
-	} else if isFifo(mode) {
-		// TODO: Fifo
-	} else if isSocket(mode) {
-		// TODO: Socket
-	} else if isTemporary(mode) {
-		// TODO: TempFile
-	}
-	return
-}
-
-func (p PathStr) Open() (*os.File, error) {
-	return os.Open(string(p))
-}
-
-func (p PathStr) WithOpen(cb func(*os.File) error) error { // FIXME: name
-	f, err := p.Open()
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	return cb(f)
-}
-
-func (p PathStr) Clean() PathStr {
-	return PathStr(filepath.Clean(string(p)))
-}
-
-// Abs implements PurePath.
-// See [path/filepath.Abs] for more details:
-//
-// > Abs returns an absolute representation of path. If the path is not absolute
-// it will be joined with the current working directory to turn it into an
-// absolute path. The absolute path name for a given file is not guaranteed to
-// be unique. Abs calls [path/filepath.Clean] on the result.
-func (p PathStr) Abs() (PathStr, error) {
-	q, err := filepath.Abs(string(p))
-	return PathStr(q), err
-}
-
-// Localize implements PurePath.
-func (p PathStr) Localize() (PathStr, error) {
-	q, err := filepath.Localize(string(p))
-	return PathStr(q), err
-}
-
-// Rel implements PurePath. See [path/filepath.Rel]:
-//
-// > Rel returns a relative path that is lexically equivalent to [target] when
-// joined to basepath with an intervening separator. That is,
-// [path/filepath.Join](basepath, Rel(basepath, target)) is equivalent to target
-// itself. On success, the returned path will always be relative to basepath,
-// even if basepath and target share no elements. An error is returned if target
-// can't be made relative to basepath or if knowing the current working directory
-// would be necessary to compute it. Rel calls [path/filepath.Clean] on the result.
-func (p PathStr) Rel(target Dir) (PathStr, error) {
-	result, err := filepath.Rel(string(p), string(target))
-	if err != nil {
-		return "", errors.Join(err, errors.New("unable to make relative path"))
-	}
-	return PathStr(result), nil
-}
+// PurePath --------------------------------------------------------------------
+var _ PurePath = PathStr(".")
 
 // A wrapper around [path/filepath.Join]:
 //
@@ -180,30 +85,121 @@ func (p PathStr) Ext() string {
 	return filepath.Ext(string(p))
 }
 
-var homeDir string
+// Returns true if the path is absolute, false otherwise.
+// See [filepath.IsAbs] for more details.
+func (p PathStr) IsAbsolute() bool {
+	return filepath.IsAbs(string(p))
+}
 
-// caches the user's home directory. Returns an empty string if it cannot be determined.
-func getHomeDir() (home string, err error) {
-	if homeDir != "" {
-		home = homeDir
-	} else {
-		home, err = os.UserHomeDir()
-		if err == nil {
-			homeDir = home
-		}
+// returns true if the path is local/relative, false otherwise.
+// see [filepath.IsLocal] for more details.
+func (p PathStr) IsLocal() bool {
+	return filepath.IsLocal(string(p))
+}
+
+// Readable --------------------------------------------------------------------
+var _ Readable[any] = PathStr(".")
+
+func (p PathStr) Read() (result any, err error) {
+	// can't define this switch as a method of OnDisk[P] since OnDisk[P] has to handle
+	// any kind of path
+	var actual OnDisk[PathStr]
+	actual, err = p.OnDisk()
+	if err != nil {
+		return
+	}
+	mode := actual.Mode()
+
+	if mode.IsRegular() {
+		result, err = os.ReadFile(string(p))
+	} else if mode.IsDir() {
+		result, err = Dir(p).Read()
+	} else if isSymLink(mode) {
+		result, err = Symlink(p).Read()
+	} else if isCharDevice(mode) {
+		// TODO: CharDevice
+	} else if isDevice(mode) {
+		// TODO: BlockDevice
+	} else if isFifo(mode) {
+		// TODO: Fifo
+	} else if isSocket(mode) {
+		// TODO: Socket
+	} else if isTemporary(mode) {
+		// TODO: TempFile
 	}
 	return
+}
+
+func (p PathStr) Open() (*os.File, error) {
+	return os.Open(string(p))
+}
+
+// func (p PathStr) WithOpen(cb func(*os.File) error) error { // FIXME: name
+// 	f, err := p.Open()
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer f.Close()
+// 	return cb(f)
+// }
+
+// Transformer -----------------------------------------------------------------
+var _ Transformer[PathStr] = PathStr(".")
+
+// Clean implements Transformer
+func (p PathStr) Clean() PathStr {
+	return PathStr(filepath.Clean(string(p)))
+}
+
+// Abs implements Transformer.
+// See [path/filepath.Abs] for more details:
+//
+// > Abs returns an absolute representation of path. If the path is not absolute
+// it will be joined with the current working directory to turn it into an
+// absolute path. The absolute path name for a given file is not guaranteed to
+// be unique. Abs calls [path/filepath.Clean] on the result.
+func (p PathStr) Abs() (PathStr, error) {
+	q, err := filepath.Abs(string(p))
+	return PathStr(q), err
+}
+
+// Localize implements Transformer.
+func (p PathStr) Localize() (PathStr, error) {
+	q, err := filepath.Localize(string(p))
+	return PathStr(q), err
+}
+
+// Rel implements Transformer. See [path/filepath.Rel]:
+//
+// > Rel returns a relative path that is lexically equivalent to [target] when
+// joined to basepath with an intervening separator. That is,
+// [path/filepath.Join](basepath, Rel(basepath, target)) is equivalent to target
+// itself. On success, the returned path will always be relative to basepath,
+// even if basepath and target share no elements. An error is returned if target
+// can't be made relative to basepath or if knowing the current working directory
+// would be necessary to compute it. Rel calls [path/filepath.Clean] on the result.
+func (p PathStr) Rel(target Dir) (PathStr, error) {
+	result, err := filepath.Rel(string(p), string(target))
+	if err != nil {
+		return "", errors.Join(err, errors.New("unable to make relative path"))
+	}
+	return PathStr(result), nil
 }
 
 // Expand a leading "~" into the user's home directory. If the home directory cannot be
 // determined, the path is returned unchanged.
 func (p PathStr) ExpandUser() (PathStr, error) {
 	if len(p) > 0 && p[0] == '~' {
-		if home, err := getHomeDir(); home != "" && err == nil {
+		if home, err := UserHomeDir(); home != "" && err == nil {
 			return PathStr(PathStr(home) + p[1:]), nil // FIXME: check p[2] == "/"
 		}
 	}
 	return p, nil
+}
+
+func UserHomeDir() (Dir, error) {
+	dir, err := os.UserHomeDir()
+	return Dir(dir), err
 }
 
 func (p PathStr) Eq(q PathStr) bool {
