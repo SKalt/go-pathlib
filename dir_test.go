877efb25251da86f7ea4002ca74c9c5c1677a15e
Steven Kalt <kalt.steven@gmail.com>
2025-08-10 19:14:11 -0400

    fix: fallible global-var-lookup-functions shoudl return Result[T]s
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch main
    # Your branch is up to date with 'origin/main'.
    #
    # Changes to be
    
    committed:
    #
    modified:   dir_test.go
    #
    modified:   globals.go
    #
    # Changes not staged for
    commit:
    #
    deleted:    .config/git/config
    #
    modified:   .golangci.yaml
    #
    modified:   ARCHITECTURE.md
    #
    modified:   README.md
    #
    modified:   dir.go
    #
    modified:   example_test.go
    #
    modified:   lib.go
    #
    modified:   on_disk.go
    #
    modified:   path_str.go
    #
    modified:   path_str_test.go
    #
    modified:   regular_file.go
    #
    modified:   result.go
    #
    modified:   symlink.go
    #
    deleted:    utils.go
    #
    modified:   wrappers.go
    #
    # Untracked
    files:
    #	.vscode/_.code-snippets
    #

--- dir_test.go
+++ dir_test.go
@@ -1,16 +1,15 @@
 package pathlib_test
 
 import (
+	"errors"
 	"fmt"
+	"io/fs"
 
 	"github.com/skalt/pathlib.go"
 )
 
 func ExampleDir_Eq() {
-	cwd, err := pathlib.Cwd()
-	if err != nil {
-		panic(err)
-	}
+	cwd := pathlib.Cwd().Unwrap()
 	fmt.Println(cwd.Eq("."))
 	fmt.Println(pathlib.Dir("/foo").Eq("/foo"))
 	fmt.Println(cwd.Join("./relative").Eq("relative"))
@@ -36,7 +35,7 @@ func ExampleDir_Glob() {
 		AsDir().
 		MakeAll(0o777, 0o777).Unwrap()
 
-	defer demoDir.RemoveAll()
+	defer func() { _ = demoDir.RemoveAll() }()
 
 	for _, name := range []string{"x", "y", "z", "a", "b", "c"} {
 		demoDir.Join(name + ".txt").AsFile().Make(0o777).Unwrap()
@@ -53,4 +52,36 @@ func ExampleDir_Glob() {
 	// /tmp/glob-example/x.txt
 	// /tmp/glob-example/y.txt
 	// /tmp/glob-example/z.txt
+}
+
+func ExampleDir_Lstat() {
+	tmpDir := pathlib.TempDir().
+		Join("dir-lstat-example").
+		AsDir().
+		MakeAll(0o777, 0o777).
+		Unwrap()
+	defer tmpDir.RemoveAll()
+
+	nonExistent := tmpDir.Join("dir").AsDir()
+	_, err := nonExistent.Lstat().Unpack()
+	if errors.Is(err, fs.ErrNotExist) {
+		fmt.Printf("%T(%q).Lstat() => fs.ErrNotExist\n", nonExistent, nonExistent)
+	}
+
+	dir := nonExistent.Make(0o777).Unwrap()
+	onDisk := dir.Lstat().Unwrap()
+	fmt.Printf("%T(%q) created with mode %s\n", dir, dir, onDisk.Mode())
+
+	file := tmpDir.Join("file.txt").AsFile()
+	file.Make(0o755).Unwrap()
+
+	_, err = pathlib.Dir(file.String()).Lstat().Unpack()
+
+	if e, ok := err.(pathlib.WrongTypeOnDisk[pathlib.Dir]); ok {
+		fmt.Println(e.Error())
+	}
+	// Output:
+	// pathlib.Dir("/tmp/dir-lstat-example/dir").Lstat() => fs.ErrNotExist
+	// pathlib.Dir("/tmp/dir-lstat-example/dir") created with mode drwxr-xr-x
+	// pathlib.Dir("/tmp/dir-lstat-example/file.txt") unexpectedly has mode -rwxr-xr-x on-disk
 }
--- globals.go
+++ globals.go
@@ -3,27 +3,27 @@ package pathlib
 import "os"
 
 // Gets the Current Working Directory. See [os.Getwd].
-func Cwd() (Dir, error) {
+func Cwd() Result[Dir] {
 	dir, err := os.Getwd()
-	return Dir(dir), err
+	return Result[Dir]{Dir(dir), err}
 }
 
 // See [os.UserHomeDir].
-func UserHomeDir() (Dir, error) {
+func UserHomeDir() Result[Dir] {
 	dir, err := os.UserHomeDir()
-	return Dir(dir), err
+	return Result[Dir]{Dir(dir), err}
 }
 
 // See [os.UserCacheDir].
-func UserCacheDir() (Dir, error) {
+func UserCacheDir() Result[Dir] {
 	dir, err := os.UserCacheDir()
-	return Dir(dir), err
+	return Result[Dir]{Dir(dir), err}
 }
 
 // See [os.UserConfigDir].
-func UserConfigDir() (Dir, error) {
+func UserConfigDir() Result[Dir] {
 	dir, err := os.UserConfigDir()
-	return Dir(dir), err
+	return Result[Dir]{Dir(dir), err}
 }
 
 // returns the process/os-wide temporary directory. See [os.TempDir].
